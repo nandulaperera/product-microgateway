9922455c575711ebca5abcfec5cc2a1a984e77fd
Nandula Perera <35824376+nandulaperera@users.noreply.github.com>
2022-01-24 23:20:31 +0530

    Add unit tests for source watcher

---
+++ adapter/internal/sourcewatcher/sourcewatcher_test.go
@@ -0,0 +1,175 @@
+package sourcewatcher
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/wso2/product-microgateway/adapter/config"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/stretchr/testify/assert"
+)
+
+// Test the fetchArtifacts method for a public repository
+func TestFetchArtifacts(t *testing.T) {
+	conf, _ := config.ReadConfigs()
+
+	dir, err := ioutil.TempDir("", "test")
+
+	defer os.RemoveAll(dir)
+
+	if err != nil {
+		t.Error("Error creating temp directory." + err.Error())
+	}
+
+	conf.Adapter.ArtifactsDirectory = dir
+	conf.Adapter.SourceControl.Repository.Username = ""
+	conf.Adapter.SourceControl.Repository.AccessToken = ""
+	conf.Adapter.SourceControl.Repository.URL = "https://github.com/wso2/product-microgateway"
+
+	repository, err := fetchArtifacts(conf)
+
+	assert.NotNil(t, repository, "Fetching artifacts failed")
+	assert.Nil(t, err, "Error fetching artifacts")
+}
+
+func TestFetchArtifactsForExistingRepository(t *testing.T) {
+	conf, _ := config.ReadConfigs()
+
+	dir, err := ioutil.TempDir("", "test")
+
+	defer os.RemoveAll(dir)
+
+	if err != nil {
+		t.Error("Error creating temp directory." + err.Error())
+	}
+
+	conf.Adapter.ArtifactsDirectory = dir
+	conf.Adapter.SourceControl.Repository.Username = ""
+	conf.Adapter.SourceControl.Repository.AccessToken = ""
+	conf.Adapter.SourceControl.Repository.URL = "https://github.com/wso2/product-microgateway"
+
+	repository, err := fetchArtifacts(conf)
+
+	if err != nil {
+		t.Error("Error fetching artifacts")
+	}
+
+	repository, err = fetchArtifacts(conf)
+
+	assert.NotNil(t, repository, "Fetching artifacts failed")
+	assert.Nil(t, err, "Error fetching artifacts")
+
+}
+
+// Test the fetchArtifacts method for a public repository with invalid credentials
+func TestFetchArtifactsWithInvalidCredentials(t *testing.T) {
+	conf, _ := config.ReadConfigs()
+
+	dir, err := ioutil.TempDir("", "test")
+
+	defer os.RemoveAll(dir)
+
+	if err != nil {
+		t.Error("Error creating temp directory." + err.Error())
+	}
+
+	conf.Adapter.ArtifactsDirectory = dir
+	conf.Adapter.SourceControl.Repository.Username = "admin"
+	conf.Adapter.SourceControl.Repository.AccessToken = "admin"
+	conf.Adapter.SourceControl.Repository.URL = "https://github.com/wso2/product-microgateway"
+
+	_, err = fetchArtifacts(conf)
+
+	assert.NotNil(t, err, "Error fetching artifacts")
+}
+
+// Test the fetchArtifacts method for an invalid repository
+func TestFetchArtifactsWithInvalidRepository(t *testing.T) {
+	conf, _ := config.ReadConfigs()
+
+	dir, err := ioutil.TempDir("", "test")
+
+	defer os.RemoveAll(dir)
+
+	if err != nil {
+		t.Error("Error creating temp directory." + err.Error())
+	}
+
+	conf.Adapter.ArtifactsDirectory = dir
+	conf.Adapter.SourceControl.Repository.Username = ""
+	conf.Adapter.SourceControl.Repository.AccessToken = ""
+	conf.Adapter.SourceControl.Repository.URL = "https://github.com/wso2/invalid-repository"
+
+	_, err = fetchArtifacts(conf)
+
+	assert.NotNil(t, err, "Fetching artifacts failed")
+}
+
+// Test the getAuth method without credentials
+func TestGetAuthWithoutCredentials(t *testing.T) {
+	conf, _ := config.ReadConfigs()
+
+	conf.Adapter.SourceControl.Repository.Username = ""
+	conf.Adapter.SourceControl.Repository.AccessToken = ""
+
+	auth := getAuth(conf)
+
+	expectedAuth := &http.BasicAuth{}
+
+	assert.Equal(t, expectedAuth, auth, "Invalid auth")
+}
+
+// Test the getAuth method with credentials
+func TestGetAuthWithCredentials(t *testing.T) {
+	conf, _ := config.ReadConfigs()
+
+	conf.Adapter.SourceControl.Repository.Username = "admin"
+	conf.Adapter.SourceControl.Repository.AccessToken = "admin"
+
+	actualAuth := getAuth(conf)
+
+	expectedAuth := &http.BasicAuth{
+		Username: "admin",
+		Password: "admin",
+	}
+
+	assert.Equal(t, expectedAuth, actualAuth, "Invalid auth")
+}
+
+// Test the backupArtifacts method
+func TestBackupArtifacts(t *testing.T){
+	dir, err := ioutil.TempDir("", "test")
+	defer os.RemoveAll(dir)
+
+	if err != nil {
+		t.Error("Error creating temp directory." + err.Error())
+	}
+
+	err = os.Mkdir(filepath.Join(dir, apisArtifactDir), 0777)
+
+	if err != nil {
+		t.Error("Error creating apis directory." + err.Error())
+	}
+
+	file, err := os.Create(filepath.Join(dir, apisArtifactDir, "test.txt"))
+
+	if err != nil {
+		t.Error("Error creating temp file." + err.Error())
+	}
+
+	defer file.Close()
+
+	conf, err := config.ReadConfigs()
+
+	if err != nil {
+		t.Error("Error reading configs." + err.Error())
+	}
+
+	conf.Adapter.ArtifactsDirectory = dir
+
+	err = backupArtifacts(conf)
+
+	assert.Nil(t, err, "Error backing up artifacts")
+}
